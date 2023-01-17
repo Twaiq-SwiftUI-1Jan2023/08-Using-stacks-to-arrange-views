2904bda54631d03cb822e54507b7de8c78410c53
batoolaj2000 <94515763+batoolaj2000@users.noreply.github.com>
2023-01-17 22:53:43 +0300

    Add files via upload

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Batool on 17/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
