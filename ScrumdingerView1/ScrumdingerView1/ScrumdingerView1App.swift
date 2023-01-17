5432bc17e7d926ca2196cd8ca19c77b14256a187
fatma-f <fatmahalharbi7@gmail.com>
2023-01-18 02:13:32 +0300

    push

---
+++ ScrumdingerView1/ScrumdingerView1/ScrumdingerView1App.swift
@@ -0,0 +1,18 @@
+//
+//  ScrumdingerView1App.swift
+//  ScrumdingerView1
+//
+//  Created by Fatma Alharbi on 25/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerView1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+            SwiftUIView1()
+        }
+    }
+}
