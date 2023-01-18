bd9958bbae0926e0904b97e2bf75e1fe781ab63c
Mercury <mercury-inc@hotmail.com>
2023-01-18 09:53:19 +0300

    my project

---
+++ Project 1/Project 1/Project_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Project_1App.swift
+//  Project 1
+//
+//  Created by RMP on 25/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct Project_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
