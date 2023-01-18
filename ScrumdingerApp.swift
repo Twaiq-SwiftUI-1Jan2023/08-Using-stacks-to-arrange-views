34985110f55d1dba4ee9e192ebfa8d106a353333
Raneem AlRashoud <439200695@student.ksu.edu.sa>
2023-01-18 09:37:37 +0300

    MeetingView

---
+++ ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Raneem AlRashoud on 24/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
