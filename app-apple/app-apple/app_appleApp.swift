e8686da6b71a188027fc717527c895951cd15e08
Mansouraldhwyan <94120818+Mansouraldhwyan@users.noreply.github.com>
2023-01-18 07:45:29 +0300

    Add files via upload

---
+++ app-apple/app-apple/app_appleApp.swift
@@ -0,0 +1,17 @@
+//
+//  app_appleApp.swift
+//  app-apple
+//
+//  Created by Abudulaziz on 25/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct app_appleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
