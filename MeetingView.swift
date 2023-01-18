25d0cf5d6a5232c185f94b078987d39827f98324
Raneem AlRushud <59771760+RaneemAlRushud@users.noreply.github.com>
2023-01-18 09:47:59 +0300

    Update indentions

--- MeetingView.swift
+++ MeetingView.swift
@@ -25,24 +25,24 @@ struct MeetingView: View {
                         .font(.caption)
                     Label("600", systemImage: "hourglass.tophalf.fill")
                 }
-              }
-              .accessibilityElement(children: .ignore)
-              .accessibilityLabel("Time remaining")
-             .accessibilityValue("10 minutes")
+            }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel("Time remaining")
+            .accessibilityValue("10 minutes")
             Circle()
                 .strokeBorder(lineWidth: 24)
             HStack {
                 Text("Speaker 1 of 3")
                 Spacer()
-                 Button(action: {}) {
-                     Image(systemName: "forward.fill")
-                 }
-                    .accessibilityLabel("Next speaker")
+                Button(action: {}) {
+                    Image(systemName: "forward.fill")
+                }
+                .accessibilityLabel("Next speaker")
             }
         }
         .padding()
         
-  
+        
     }
 }
 
