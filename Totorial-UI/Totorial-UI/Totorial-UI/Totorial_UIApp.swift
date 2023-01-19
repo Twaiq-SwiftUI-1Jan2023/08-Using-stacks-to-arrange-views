53f5c63ca7fe8f269b4b8daf94202005d10164fc
Judy Alshahrani <judy@alMacBook-Air-alkhas-bJudy.local>
2023-01-19 15:07:24 +0300

    pp

---
+++ Totorial-UI/Totorial-UI/Totorial-UI/Totorial_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Totorial_UIApp.swift
+//  Totorial-UI
+//
+//  Created by Judy Alshahrani on 24/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct Totorial_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
